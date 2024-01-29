93c47bbe4d11bad537c94f45cff9650f5175638d
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-01-29 15:30:12 +0100

    get user by id

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/users.schema';
@@ -21,8 +21,12 @@ export class UsersService {
     return this.userModel.find() ;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) : Promise <User| null > {
+    const user = await this.userModel.findOne({_id : id}).exec();
+     if (!user) {
+      throw new NotFoundException('User not found');
+    } ;
+    return user ;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
